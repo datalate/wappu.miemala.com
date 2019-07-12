0d120f94660287e0767d0efb8d3cc5674a83a14d
Lauri Haavisto <lauri.haavisto@student.tut.fi>
2019-07-12 19:39:57 +0300

    Fix build script and add required typings

--- wappu-api/util/build.js
+++ wappu-api/util/build.js
@@ -1,10 +1,17 @@
 const fs = require('fs-extra');
 const childProcess = require('child_process');
+const spawn = require('child_process').spawn;
 
 // remove current build, and create new one
 fs.removeSync('./dist/');
 
-// TODO: fix build
-
 // transpile the typescript files
-childProcess.exec('tsc --build tsconfig.prod.json');
+tsc = spawn('tsc', ['--build', 'tsconfig.prod.json']);
+tsc.stdout.on('data', data => {
+  console.log(data.toString());
+});
+
+tsc.stderr.on('data', data => {
+  console.err(data.toString());
+});
+
