7207064688e83ca6323eff842e24238e5a83cff3
Lauri Haavisto <lauri.haavisto@student.tut.fi>
2019-07-12 19:45:02 +0300

    Remove deprecated operators aliases

--- wappu-api/src/db/sequelize.ts
+++ wappu-api/src/db/sequelize.ts
@@ -1,5 +1,4 @@
 import {Sequelize} from 'sequelize-typescript';
-import {Op} from 'sequelize';
 
 // const env = process.env.NODE_ENV || "development";
 // const config = require("/../config.json")[env];
@@ -9,7 +8,6 @@ import {Op} from 'sequelize';
 
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
-    operatorsAliases: Op,
     database: 'wappu-api',
     storage: ':memory:',
     models: [__dirname + '/models']
