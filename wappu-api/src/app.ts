e5241c7c7688b2578f955082540b0058179fe70c
Lauri Haavisto <lauri.haavisto@miemala.com>
2019-07-13 17:37:40 +0300

    Add CORS support to API and rename morgan import

--- wappu-api/src/app.ts
+++ wappu-api/src/app.ts
@@ -1,13 +1,16 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
-import logger from 'morgan';
+import morgan from 'morgan';
 import BaseRouter from './routes/base';
 
 export const app = express();
 
 // Add middleware/settings/routes to express.
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(cookieParser());
+
 app.use(BaseRouter.path, BaseRouter.router);
