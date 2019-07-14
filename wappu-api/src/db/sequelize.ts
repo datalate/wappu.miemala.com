1611aee8cb0c054df54f82ffcb20fcbacca864da
Lauri Haavisto <lauri.haavisto@miemala.com>
2019-07-14 13:21:45 +0300

    Use actual database instead of in-memory db

--- wappu-api/src/db/sequelize.ts
+++ wappu-api/src/db/sequelize.ts
@@ -9,6 +9,7 @@ import {Sequelize} from 'sequelize-typescript';
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
     database: 'wappu-api',
-    storage: ':memory:',
-    models: [__dirname + '/models']
+    // storage: ':memory:',
+    storage: 'database.sqlite',
+    models: [__dirname + '/models'],
 });
