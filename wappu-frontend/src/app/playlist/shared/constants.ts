5c0cdbe47da9b34a8af6019d18ba44a604824f48
Lauri Haavisto <lauri.haavisto@miemala.com>
2019-08-15 18:58:09 +0300

    Show 2016 and 2017 radio (this time for real)

--- wappu-frontend/src/app/playlist/shared/constants.ts
+++ wappu-frontend/src/app/playlist/shared/constants.ts
@@ -1,11 +1,11 @@
 import { Radio } from '../../core/models';
 
 export const RADIO_EDITIONS: Radio[] = [
-// {id: '2016', startAt: new Date('2016-04-10T00:00:00+03:00'), endAt: new Date('2016-04-31T23:59:59+03:00')},
-// {id: '2017', startAt: new Date('2017-04-10T00:00:00+03:00'), endAt: new Date('2017-04-31T23:59:59+03:00')},
+  {id: '2016', startAt: new Date('2016-04-19T00:00:00+03:00'), endAt: new Date('2016-04-30T23:59:59+03:00')},
+  {id: '2017', startAt: new Date('2017-04-19T00:00:00+03:00'), endAt: new Date('2017-04-30T23:59:59+03:00')},
   {id: '2018', startAt: new Date('2018-04-17T00:00:00+03:00'), endAt: new Date('2018-04-30T23:59:59+03:00')},
   {id: 'ruska', startAt: new Date('2018-10-22T00:00:00+03:00'), endAt: new Date('2018-10-25T23:59:59+03:00')},
-  {id: '2019', startAt: new Date('2019-04-15T00:00:00+03:00'), endAt: new Date('2019-04-31T23:59:59+03:00')},
+  {id: '2019', startAt: new Date('2019-04-15T00:00:00+03:00'), endAt: new Date('2019-04-30T23:59:59+03:00')},
   {id: 'assyt', startAt: new Date('2019-08-01T00:00:00+03:00'), endAt: new Date('2019-08-04T23:59:59+03:00')},
 ];
 
