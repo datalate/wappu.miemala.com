2bbb64a6a675ab966e4f7059bf1950dbdbb64016
Lauri Haavisto <lauri.haavisto@miemala.com>
2021-05-10 22:43:32 +0300

    Update latest radio to 2021

--- wappu-frontend/src/app/playlist/shared/constants.ts
+++ wappu-frontend/src/app/playlist/shared/constants.ts
@@ -7,7 +7,8 @@ export const RADIO_EDITIONS: Radio[] = [
   { id: 'ruska', startAt: new Date('2018-10-22T00:00:00+03:00'), endAt: new Date('2018-10-25T23:59:59+03:00') },
   { id: '2019', startAt: new Date('2019-04-15T00:00:00+03:00'), endAt: new Date('2019-04-30T23:59:59+03:00') },
   { id: 'assyt', startAt: new Date('2019-08-01T00:00:00+03:00'), endAt: new Date('2019-08-04T23:59:59+03:00') },
-  { id: '2020', startAt: new Date('2020-04-15T00:00:00+03:00'), endAt: new Date('2020-04-30T23:59:59+03:00') }
+  { id: '2020', startAt: new Date('2020-04-15T00:00:00+03:00'), endAt: new Date('2020-04-30T23:59:59+03:00') },
+  { id: '2021', startAt: new Date('2021-04-15T00:00:00+03:00'), endAt: new Date('2021-04-30T23:59:59+03:00') }
 ];
 
-export const LATEST_RADIO = '2020';
+export const LATEST_RADIO = '2021';
